a4cd8032c7f51430f8b3a5cd6103a2c012d2e391
Alec Jang <ayj325@lehigh.edu>
2025-03-05 23:58:59 -0500

    [IMPORTANT] Fixes for running the tests/metrics on the test.txt clips (rather than val.txt clips)

--- DVIS_Plus/utils/eval_miou_vspw.py
+++ DVIS_Plus/utils/eval_miou_vspw.py
@@ -4,89 +4,214 @@ from PIL import Image
 import sys
 from tqdm import tqdm
 
+# class Evaluator(object):
+#     def __init__(self, num_class):
+#         self.num_class = num_class
+#         self.confusion_matrix = np.zeros((self.num_class,)*2)
+
+#     def beforeval(self):
+#         isval = np.sum(self.confusion_matrix,axis=1)>0
+#         self.confusion_matrix = self.confusion_matrix*isval
+
+#     def Pixel_Accuracy(self):
+#         Acc = np.diag(self.confusion_matrix).sum() / self.confusion_matrix.sum()
+#         return Acc
+
+#     def Pixel_Accuracy_Class(self):
+#         Acc = np.diag(self.confusion_matrix) / self.confusion_matrix.sum(axis=1)
+#         Acc = np.nanmean(Acc)
+#         return Acc
+
+
+#     def Mean_Intersection_over_Union(self):
+#         MIoU = np.diag(self.confusion_matrix) / (
+#                     np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0) -
+#                     np.diag(self.confusion_matrix))
+#         isval = np.sum(self.confusion_matrix,axis=1)>0
+#         MIoU = np.nansum(MIoU*isval)/isval.sum()
+#         return MIoU
+
+#     def Frequency_Weighted_Intersection_over_Union(self):
+#         freq = np.sum(self.confusion_matrix, axis=1) / np.sum(self.confusion_matrix)
+#         iu = np.diag(self.confusion_matrix) / (
+#                     np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0) -
+#                     np.diag(self.confusion_matrix))
+
+#         FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
+#         return FWIoU
+
+#     def _generate_matrix(self, gt_image, pre_image):
+#         # for vspw label preprocessing
+#         gt_image[gt_image == 0] = 255
+#         gt_image = gt_image - 1
+
+#         mask = (gt_image >= 0) & (gt_image < self.num_class)
+#         #print(mask)
+#         #print(gt_image.shape)
+#         #print(gt_image[mask])
+#         label = self.num_class * gt_image[mask].astype('int') + pre_image[mask]
+# #        print(label.shape)
+#         count = np.bincount(label, minlength=self.num_class**2)
+#         confusion_matrix = count.reshape(self.num_class, self.num_class)
+#         return confusion_matrix
+
+#     def add_batch(self, gt_image, pre_image):
+#         assert gt_image.shape == pre_image.shape
+#         self.confusion_matrix += self._generate_matrix(gt_image, pre_image)
+
+#     def reset(self):
+#         self.confusion_matrix = np.zeros((self.num_class,) * 2)
+
+
+# eval_ = Evaluator(124)
+# eval_.reset()
+
+# DIR=sys.argv[1]
+# split = 'val.txt'
+
+# with open(os.path.join(DIR,split),'r') as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         videolist = [line[:-1] for line in lines]
+# PRED=sys.argv[2]
+# for video in tqdm(videolist):
+#     for tar in os.listdir(os.path.join(DIR,'data',video,'mask')):
+#         pred = os.path.join(PRED,video,tar)
+#         tar_ = Image.open(os.path.join(DIR,'data',video,'mask',tar))
+#         tar_ = np.array(tar_)
+#         tar_ = tar_[np.newaxis,:]
+#         pred_ = Image.open(pred)
+#         pred_ = np.array(pred_)
+#         pred_ = pred_[np.newaxis,:]
+#         eval_.add_batch(tar_,pred_)
+
+# Acc = eval_.Pixel_Accuracy()
+# Acc_class = eval_.Pixel_Accuracy_Class()
+# mIoU = eval_.Mean_Intersection_over_Union()
+# FWIoU = eval_.Frequency_Weighted_Intersection_over_Union()
+# print("Acc:{}, Acc_class:{}, mIoU:{}, fwIoU: {}".format(Acc, Acc_class, mIoU, FWIoU))
+
+
+
 class Evaluator(object):
     def __init__(self, num_class):
         self.num_class = num_class
-        self.confusion_matrix = np.zeros((self.num_class,)*2)
+        self.confusion_matrix = np.zeros((self.num_class,) * 2)
 
     def beforeval(self):
-        isval = np.sum(self.confusion_matrix,axis=1)>0
-        self.confusion_matrix = self.confusion_matrix*isval
+        isval = np.sum(self.confusion_matrix, axis=1) > 0
+        self.confusion_matrix = self.confusion_matrix * isval
 
     def Pixel_Accuracy(self):
-        Acc = np.diag(self.confusion_matrix).sum() / self.confusion_matrix.sum()
-        return Acc
+        try:
+            Acc = np.diag(self.confusion_matrix).sum() / self.confusion_matrix.sum()
+            return Acc
+        except ZeroDivisionError:
+            print("Error in Pixel_Accuracy: Division by zero")
+            return 0
 
     def Pixel_Accuracy_Class(self):
-        Acc = np.diag(self.confusion_matrix) / self.confusion_matrix.sum(axis=1)
-        Acc = np.nanmean(Acc)
-        return Acc
-
+        try:
+            Acc = np.diag(self.confusion_matrix) / self.confusion_matrix.sum(axis=1)
+            Acc = np.nanmean(Acc)
+            return Acc
+        except ZeroDivisionError:
+            print("Error in Pixel_Accuracy_Class: Division by zero")
+            return 0
 
     def Mean_Intersection_over_Union(self):
-        MIoU = np.diag(self.confusion_matrix) / (
-                    np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0) -
-                    np.diag(self.confusion_matrix))
-        isval = np.sum(self.confusion_matrix,axis=1)>0
-        MIoU = np.nansum(MIoU*isval)/isval.sum()
-        return MIoU
+        try:
+            MIoU = np.diag(self.confusion_matrix) / (
+                np.sum(self.confusion_matrix, axis=1)
+                + np.sum(self.confusion_matrix, axis=0)
+                - np.diag(self.confusion_matrix)
+            )
+            isval = np.sum(self.confusion_matrix, axis=1) > 0
+            MIoU = np.nansum(MIoU * isval) / isval.sum()
+            return MIoU
+        except ZeroDivisionError:
+            print("Error in Mean_Intersection_over_Union: Division by zero")
+            return 0
 
     def Frequency_Weighted_Intersection_over_Union(self):
-        freq = np.sum(self.confusion_matrix, axis=1) / np.sum(self.confusion_matrix)
-        iu = np.diag(self.confusion_matrix) / (
-                    np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0) -
-                    np.diag(self.confusion_matrix))
-
-        FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
-        return FWIoU
+        try:
+            freq = np.sum(self.confusion_matrix, axis=1) / np.sum(self.confusion_matrix)
+            iu = np.diag(self.confusion_matrix) / (
+                np.sum(self.confusion_matrix, axis=1)
+                + np.sum(self.confusion_matrix, axis=0)
+                - np.diag(self.confusion_matrix)
+            )
+            FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
+            return FWIoU
+        except ZeroDivisionError:
+            print("Error in Frequency_Weighted_Intersection_over_Union: Division by zero")
+            return 0
 
     def _generate_matrix(self, gt_image, pre_image):
-        # for vspw label preprocessing
-        gt_image[gt_image == 0] = 255
-        gt_image = gt_image - 1
+        try:
+            # for vspw label preprocessing
+            gt_image[gt_image == 0] = 255
+            gt_image = gt_image - 1
 
-        mask = (gt_image >= 0) & (gt_image < self.num_class)
-        #print(mask)
-        #print(gt_image.shape)
-        #print(gt_image[mask])
-        label = self.num_class * gt_image[mask].astype('int') + pre_image[mask]
-#        print(label.shape)
-        count = np.bincount(label, minlength=self.num_class**2)
-        confusion_matrix = count.reshape(self.num_class, self.num_class)
-        return confusion_matrix
+            mask = (gt_image >= 0) & (gt_image < self.num_class)
+            label = self.num_class * gt_image[mask].astype('int') + pre_image[mask]
+            count = np.bincount(label, minlength=self.num_class**2)
+            confusion_matrix = count.reshape(self.num_class, self.num_class)
+            return confusion_matrix
+        except Exception as e:
+            print(f"Error in _generate_matrix: {e}")
+            return np.zeros((self.num_class, self.num_class))
 
     def add_batch(self, gt_image, pre_image):
-        assert gt_image.shape == pre_image.shape
-        self.confusion_matrix += self._generate_matrix(gt_image, pre_image)
+        try:
+            assert gt_image.shape == pre_image.shape, "Shape mismatch between gt_image and pre_image"
+            self.confusion_matrix += self._generate_matrix(gt_image, pre_image)
+        except AssertionError as e:
+            print(f"Error in add_batch: {e}")
+        except Exception as e:
+            print(f"Unexpected error in add_batch: {e}")
 
     def reset(self):
         self.confusion_matrix = np.zeros((self.num_class,) * 2)
 
 
+# Initialize evaluator
 eval_ = Evaluator(124)
 eval_.reset()
 
 DIR=sys.argv[1]
-split = 'val.txt'
+split = 'test.txt' # Changed to test.txt
 
-with open(os.path.join(DIR,split),'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        videolist = [line[:-1] for line in lines]
-PRED=sys.argv[2]
+try:
+    with open(os.path.join(DIR, split), 'r') as f:
+        lines = f.readlines()
+        videolist = [line.strip() for line in lines]
+except FileNotFoundError as e:
+    print(f"Error: Split file not found: {e}")
+    sys.exit(1)
+
+PRED = sys.argv[2]
 for video in tqdm(videolist):
-    for tar in os.listdir(os.path.join(DIR,'data',video,'mask')):
-        pred = os.path.join(PRED,video,tar)
-        tar_ = Image.open(os.path.join(DIR,'data',video,'mask',tar))
-        tar_ = np.array(tar_)
-        tar_ = tar_[np.newaxis,:]
-        pred_ = Image.open(pred)
-        pred_ = np.array(pred_)
-        pred_ = pred_[np.newaxis,:]
-        eval_.add_batch(tar_,pred_)
+    try:
+        for tar in os.listdir(os.path.join(DIR, 'data', video, 'mask')):
+            pred = os.path.join(PRED, video, tar)
+            tar_ = Image.open(os.path.join(DIR, 'data', video, 'mask', tar))
+            tar_ = np.array(tar_)
+            tar_ = tar_[np.newaxis, :]
+            pred_ = Image.open(pred)
+            pred_ = np.array(pred_)
+            pred_ = pred_[np.newaxis, :]
+            eval_.add_batch(tar_, pred_)
+    except FileNotFoundError as e:
+        print(f"File not found for video {video}: {e}")
+    except Exception as e:
+        print(f"Error processing video {video}: {e}")
 
-Acc = eval_.Pixel_Accuracy()
-Acc_class = eval_.Pixel_Accuracy_Class()
-mIoU = eval_.Mean_Intersection_over_Union()
-FWIoU = eval_.Frequency_Weighted_Intersection_over_Union()
-print("Acc:{}, Acc_class:{}, mIoU:{}, fwIoU: {}".format(Acc, Acc_class, mIoU, FWIoU))
+try:
+    Acc = eval_.Pixel_Accuracy()
+    Acc_class = eval_.Pixel_Accuracy_Class()
+    mIoU = eval_.Mean_Intersection_over_Union()
+    FWIoU = eval_.Frequency_Weighted_Intersection_over_Union()
+    print(f"Acc: {Acc}, Acc_class: {Acc_class}, mIoU: {mIoU}, fwIoU: {FWIoU}")
+except Exception as e:
+    print(f"Error calculating metrics: {e}")
